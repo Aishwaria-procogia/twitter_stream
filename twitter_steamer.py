5ad4852b4d2f525392f5412547ea98ba1a0b1823
Ryan <rlenaha2@gmail.com>
2019-10-08 22:12:13 -0700

    Updated AWS storage to DynamoDB instead of S3, also reduce the number of columns

--- twitter_steamer.py
+++ twitter_steamer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from  pandas.io.json import json_normalize
 import json
 import os
 import time
@@ -18,6 +19,7 @@ access_secret = os.environ['ACCESS_TOKEN_SECRET']
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
+        df_tweet = pd.DataFrame()
         all_data = json.loads(data)
 
         #Write .json file for all data fields
@@ -25,30 +27,36 @@ class StdOutListener(StreamListener):
             f.write(json.dumps(all_data))
             f.write('\n')
 
+        #for item in all_data:
+        #    print(item)
+        #    print(all_data['user'])
+
+        #print(all_data) 
+        table = client.Table('stream_tweets')
+        response = table.put_item(Item={'id':all_data['id'] if all_data['id'] else None,
+                                        'user':all_data['user']['screen_name']  if
+                                        all_data['user']['screen_name'] else None,
+                                        'created_at':all_data['created_at']  if
+                                        all_data['created_at'] else None,
+                                        'text':all_data['text']  if
+                                        all_data['text'] else None,
+                                        'batch_number':1})
+
     def on_error(self, status):
         print (status)
 
 if __name__=='__main__':
-    runtime = 20
-    with open('tweets.json', 'w') as f:
-        f = {}
-
+    runtime = 15
+    #with open('tweets.json', 'w') as f:
+    #    f = {}
+    client = boto3.resource('dynamodb')
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     stream = Stream(auth, l)
-    stream.filter(track=['aws', '#aws', 'datascience', '#datascience', '#python', 'python'], async = True)
+    stream.filter(track=['aws', '#aws', 'datascience', '#datascience',
+                         '#python', 'python', '#seahawks'], async = True)
     time.sleep(runtime) #halts the control for runtime seconds
 
     stream.disconnect()
-
-    # Create an S3 client
-    s3 = boto3.client('s3')
-
-    filename = 'tweets.json'
-    bucket_name = 'tweet-test-321'
-
-    # Uploads the given file using a managed uploader, which will split up large
-    # files automatically and upload parts in parallel.
-    s3.upload_file(filename, bucket_name, filename)
 
